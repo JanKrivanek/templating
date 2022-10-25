ff94ed204dd9864bc0b7b8a8cf612395766b376f
Jan Krivanek <jankrivanek@microsoft.com>
2022-10-25 13:52:24 +0200

    Add Demo test cases

--- test/Microsoft.TemplateEngine.Authoring.TemplateVerifier.IntegrationTests/ExampleTemplateTest.cs
+++ test/Microsoft.TemplateEngine.Authoring.TemplateVerifier.IntegrationTests/ExampleTemplateTest.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Microsoft.TemplateEngine.TestHelper;
 using Microsoft.TemplateEngine.Tests;
@@ -47,6 +48,76 @@ namespace Microsoft.TemplateEngine.Authoring.TemplateVerifier.IntegrationTests
                                 content.Replace("SampleTestTemplate", "%TEMPLATE%");
                             }
                         }));
+
+            VerificationEngine engine = new VerificationEngine(_log);
+            await engine.Execute(options);
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+
+        [Fact]
+        public async void MyAwesomeTemplate_IsBackCompatible()
+        {
+            string templateShortName = "TestAssets.SampleTestTemplate";
+            string templateLocation = Path.Combine(TestTemplatesLocation, "TestTemplate");
+
+            TemplateVerifierOptions options = new TemplateVerifierOptions(templateName: templateShortName)
+            {
+                // Run the specific template scenario
+                TemplateSpecificArgs = new string[] { "--paramB", "true" },
+                // With local template files (or locally installed template)
+                TemplatePath = templateLocation,
+                // Capture the command outputs if needed
+                VerifyCommandOutput = true,
+                // Filter out (or allow-list) files to check
+                VerificationExcludePatterns = new[] { "*.csproj" },
+                // Decide on differentiating runs of tests
+                UniqueFor = UniqueForOption.Architecture,
+            }
+                // Scrub files content
+                .WithCustomScrubbers(
+                ScrubbersDefinition.Empty
+                    .AddScrubber(sb => sb.Replace("B is enabled", "*******"), "cs")
+                    .AddScrubber((path, content) => content.Replace("SampleTestTemplate", "%TEMPLATE%")));
+
+            VerificationEngine engine = new VerificationEngine(_log);
+            await engine.Execute(options);
+        }
+
+        [Fact]
+        public async void MyAwesomeTemplate_IsBackCompatible_CustomCheck()
+        {
+            string templateShortName = "TestAssets.SampleTestTemplate";
+            string templateLocation = Path.Combine(TestTemplatesLocation, "TestTemplate");
+
+            TemplateVerifierOptions options = new TemplateVerifierOptions(templateName: templateShortName)
+            {
+                // Run the specific template scenario
+                TemplateSpecificArgs = new string[] { "--paramB", "true" },
+                // With local template files (or locally installed template)
+                TemplatePath = templateLocation,
+                // Capture the command outputs if needed
+                // VerifyCommandOutput = true,
+                // Filter out (or allow-list) files to check
+                VerificationExcludePatterns = new[] { "*.csproj" },
+                // Decide on differentiating runs of tests
+                UniqueFor = UniqueForOption.Architecture,
+            }
+                // Scrub files content
+                .WithCustomScrubbers(
+                ScrubbersDefinition.Empty
+                    .AddScrubber(sb => sb.Replace("B is enabled", "*******"), "cs")
+                    .AddScrubber((path, content) => content.Replace("SampleTestTemplate", "%TEMPLATE%")))
+                .WithCustomDirectoryVerifier(async (directory, fetcher) =>
+                {
+                    await foreach (var (filePath, scrubbedContent) in fetcher.Value)
+                    {
+                        Path.GetExtension(filePath).Should().BeEquivalentTo(".cs");
+                        scrubbedContent.Should().Contain("*****");
+                    }
+                });
 
             VerificationEngine engine = new VerificationEngine(_log);
             await engine.Execute(options);
